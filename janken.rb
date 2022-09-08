347e88d08bb846c71444d3c49231b0b79cfe21dc
tanamaru <yt.0408.mmm@gmail.com>
2022-09-08 21:51:15 +0900

    じゃんけんの大枠システム、完成

--- janken.rb
+++ janken.rb
@@ -1,20 +1,21 @@
 def janken_hand(cpu_hand, player_hand)
-  if cpu_hand == g
+  if cpu_hand == "g"
     puts "CPU...グー"
-  elsif cpu_hand == c
+  elsif cpu_hand == "c"
     puts "CPU...チョキ"
-  elsif cpu_hand == p
+  elsif cpu_hand == "p"
     puts "CPU...パー"
   else
   end
 
-  if player_hand == g
-    puts "CPU...グー"
-  elsif player_hand == c
-    puts "CPU...チョキ"
-  elsif player_hand == p
-    puts "CPU...パー"
+  if player_hand == "g"
+    puts "あなた...グー"
+  elsif player_hand == "c"
+    puts "あなた...チョキ"
+  elsif player_hand == "p"
+    puts "あなた...パー"
   else
+    puts "入力値が間違っています。"
   end
 end
 
@@ -31,12 +32,12 @@ def janken(player_hand, cpu_hand, game_tiems)
     puts "勝ち！"
   elsif player_hand == "p" && cpu_hand == "c"
     puts "負け"
+  elsif player_hand == cpu_hand
+    puts "あいこ！"
   else
+    puts "勝負ができません。"
   end
 end
-
-
-
 
 
 puts "何本勝負？(press 1 or 3 or 5)"
@@ -46,9 +47,10 @@ puts "#{game_tiems}本勝負を選びました"
 (1..game_tiems).each do |x|
   cpu_hand = ["g", "c", "p"].sample
   puts "じゃんけん...(press g or c or p)"
-  player_hand = gets
+  player_hand = gets.chomp
   player_hand = player_hand.to_s
   puts janken_hand(cpu_hand, player_hand)
-end
 
-puts "1本目"
+  puts janken(player_hand, cpu_hand, game_tiems)
+  
+end
